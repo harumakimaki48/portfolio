4976a6ef91981521255a8ae8c6ab46d46fd67ae4
市川陽菜 <ichikawaharuna@ichikawaharunanoMacBook-Air.local>
2024-07-25 17:14:51 +0900

    portfolio#10

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Shop.create!([
+  { name: 'かさの家', opening_time: '09:00', closing_time: '17:00', holiday: '毎週月曜日', url: 'http://www.kasanoya.com/umegaemochi.html' },
+  { name: 'やす武', opening_time: '10:00', closing_time: '18:00', holiday: '毎週火曜日', url: 'https://umegaemochi.com/' }
+])
