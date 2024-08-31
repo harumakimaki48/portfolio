dce3de78537155a72425b5e46a25b4ebf4a611be
市川陽菜 <ichikawaharuna@ichikawaharunanoMacBook-Air.local>
2024-08-31 23:51:59 +0900

    ロード削除

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    onfig.middleware.delete ActionDispatch::HostAuthorization
+    config.middleware.delete ActionDispatch::HostAuthorization
   end
 end
